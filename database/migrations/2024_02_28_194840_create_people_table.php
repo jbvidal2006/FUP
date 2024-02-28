f75a66cf2f0fdac23e6fb867f2eb1c8b70888607
Ronald Rengifo <ronaldpptx@gmail.com>
2024-02-28 15:04:00 -0500

    se creo la migracion people

---
+++ database/migrations/2024_02_28_194840_create_people_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('peo_name',80);
+            $table->string('peo_lastname',80);
+            $table->string('poe_adress',100);
+            $table->integer('phone');
+            $table->date('peo_dateBirth');
+            $table->text('peo_image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('people');
+    }
+};
