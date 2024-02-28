ab557b90f8f07645ca4c4321ca972b7a9abec199
Brandon Rosero <yesidrosero2002@gmail.com>
2024-02-28 14:41:35 -0500

    se crear migraciones categories,products,providers

---
+++ database/migrations/2024_02_28_192947_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('pro_name',80);
+            $table->string('pro_type',40);
+            $table->Integer('pro_amount');
+            $table->string('pro_certs');
+            $table->text('pro_image');
+            $table->string('pro_unit');
+            $table->foreignId('providers_id')->references('id')->on('providers');
+            $table->foreignId('categories_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
