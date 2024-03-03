01f1e53349463020a164401f7ff093586791e45a
Ronald Rengifo <ronaldpptx@gmail.com>
2024-03-03 09:48:41 -0500

    se crea el controlador de request

---
+++ app/Http/Controllers/RequestsAppController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RequestApp;
+use Illuminate\Http\Request;
+
+class RequestsAppController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RequestApp $requestApp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RequestApp $requestApp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, RequestApp $requestApp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RequestApp $requestApp)
+    {
+        //
+    }
+}
