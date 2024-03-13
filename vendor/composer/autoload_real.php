89df8bd72d2d56783ab97879fcbc91e1d69e3373
Brandon Rosero <yesidrosero2002@gmail.com>
2024-03-13 15:27:42 -0500

    archivos Santum

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,18 +33,25 @@ class ComposerAutoloaderInit394bf83db0d1ea5efbce3b8133ec777e
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInit394bf83db0d1ea5efbce3b8133ec777e::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
+        $includeFiles = \Composer\Autoload\ComposerStaticInit394bf83db0d1ea5efbce3b8133ec777e::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire394bf83db0d1ea5efbce3b8133ec777e($fileIdentifier, $file);
         }
 
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire394bf83db0d1ea5efbce3b8133ec777e($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
