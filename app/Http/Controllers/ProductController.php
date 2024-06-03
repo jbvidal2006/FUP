1f38b0eba011f6f22c27a68f0f8e8cd4344a2c57
Brandon Rosero <yesidrosero2002@gmail.com>
2024-06-03 14:06:43 -0500

    controlador productos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -59,7 +59,7 @@ class ProductController extends Controller
     public function show(Product $product)
     {
 
-        return response()->json(['status' => true, 'data' => $product]);
+        return response()->json([$product]);
     }
 
     /**
